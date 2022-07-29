1146322fc436d276d1df4257e84d1132374cfa45
Chamara Singhabahu <mails.chamara@gmail.com>
2022-07-30 00:18:29 +0530

    Add VPC module

---
+++ infrastructure/modules/vpc/main.tf
@@ -0,0 +1,27 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.2"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs              = var.vpc_azs
+  private_subnets  = var.vpc_private_subnets
+  public_subnets   = var.vpc_public_subnets
+  database_subnets = var.vpc_database_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  enable_flow_log                      = true
+  create_flow_log_cloudwatch_log_group = true
+  create_flow_log_cloudwatch_iam_role  = true
+  flow_log_max_aggregation_interval    = 60
+
+  tags = {
+    Environment = "dev"
+  }
+}
---
+++ infrastructure/modules/vpc/variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_name" {
+  type    = string
+  default = "demo-eks-vpc"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "vpc_azs" {
+  type    = list(string)
+  default = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
+}
+
+variable "vpc_private_subnets" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "vpc_public_subnets" {
+  type    = list(string)
+  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
+variable "vpc_database_subnets" {
+  type    = list(string)
+  default = ["10.0.201.0/24", "10.0.202.0/24", "10.0.203.0/24"]
+}
