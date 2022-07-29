9e3966cfd821ecc94d68544a5c893c1f091a9edf
Chamara Singhabahu <mails.chamara@gmail.com>
2022-07-30 00:43:43 +0530

    Add EKS module

---
+++ infrastructure/modules/eks/output.tf
@@ -0,0 +1,24 @@
+output "cluster_arn" {
+  description = "The Amazon Resource Name (ARN) of the cluster"
+  value       = try(module.eks.cluster_arn, "")
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for your Kubernetes API server"
+  value       = try(module.eks.cluster_endpoint, "")
+}
+
+output "cluster_id" {
+  description = "The name/id of the EKS cluster. Will block on cluster creation until the cluster is really ready"
+  value       = try(module.eks.cluster_id, "")
+}
+
+output "cluster_version" {
+  description = "The Kubernetes version for the cluster"
+  value       = try(module.eks.cluster_version, "")
+}
+
+output "cluster_primary_security_group_id" {
+  description = "Cluster security group that was created by Amazon EKS for the cluster. Managed node groups use this security group for control-plane-to-data-plane communication. Referred to as 'Cluster security group' in the EKS console"
+  value       = try(module.eks.cluster_security_group_id, "")
+}
